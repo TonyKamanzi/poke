220fb805247454bb074dc03982654b8532e7104d
Kamanzi Tony <kamanzitony06@gmail.com>
2026-09-11 09:43:01 +0200

    feat: integrate Pokemon API and add Tailwind CSS support
    - Added axios for API requests and implemented fetching of Pokemon data from the PokeAPI.
    - Updated HomeScreen component to display Pokemon names, types, and images.
    - Introduced Tailwind CSS for styling with NativeWind integration.
    - Created global CSS file for Tailwind directives.
    - Configured Babel and Metro for NativeWind support.
    - Updated package.json to include new dependencies: axios, babel-preset-expo, nativewind, prettier-plugin-tailwindcss, and tailwindcss.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,72 @@
-import { Text, View, StyleSheet } from "react-native";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 
-export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+interface Pokemon {
+  name: string;
+  image: string;
+  imageBack: string;
+ types: PokemonType[];
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+interface PokemonType {
+
+  type: {
+    name: string;
+    url: string;
+  };
+}
+
+export default function HomeScreen() {
+  const [pokes, setPokes] = useState<Pokemon[]>([]);
+
+  const fetchPoke = async () => {
+    try {
+      const res = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon/?limit=20",
+      );
+
+      // fetch detailed information for each Pokemon
+      const detailedPokes = await Promise.all(
+        res.data.results.map(async (poke: any) => {
+          const pokeRes = await axios.get(poke.url);
+          return {
+            name: poke.name,
+            image: pokeRes.data.sprites.front_default,
+            imageBack: pokeRes.data.sprites.back_default,
+            types: pokeRes.data.types,
+          };
+        }),
+      );
+      setPokes(detailedPokes);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPoke();
+  }, []);
+
+  return (
+    <ScrollView>
+      {pokes.map((poke) => (
+        <View key={poke.name}>
+          <Text className="">{poke.name}</Text>
+          <Text className="">{ poke.types[0]?.type.name }</Text>
+          <View className="flex-row justify-center items-center p-4
+          ">
+            <Image
+              source={{ uri: poke.image }}
+              style={{ width: 150, height: 150 }}
+            />
+            <Image
+              source={{ uri: poke.imageBack }}
+              style={{ width: 150, height: 150 }}
+            />
+          </View>
+        </View>
+      ))}
+    </ScrollView>
+  );
+}
